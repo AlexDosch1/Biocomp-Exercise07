3b444c4d93be94d0d5c6b2232b453f2002a37ad5
AlexDosch1 <91433155+AlexDosch1@users.noreply.github.com>
2021-11-04 14:59:33 -0400

    Add files via upload

--- Challenge_1.R
+++ Challenge_1.R
@@ -1,6 +1,9 @@
 # Recreate head from bash in R *used with wages.csv
 # define a variable with the file to return lines from
-var<-read.table("wages.csv", header=TRUE, sep=",", stringsAsFactors = FALSE,)
+var<-read.table("wages.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
 # and a variable representing the number of lines to be returned from
   # the top of the indicated file
+n<-var[1:n,] #where n is a flexible number you want to print; to replicate
+  # head from bash, set n to 10
 # print selected file to terminal
+print(n)
